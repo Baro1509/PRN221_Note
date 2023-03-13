c2b8b537b5e983d603e0dc080908782fd3bafc2f
hoang bao <baohoang.7882@hotmail.com>
2023-03-13 19:21:13 +0700

    fix: add DI for controller

--- Repository/Models/NoteDbContext.cs
+++ Repository/Models/NoteDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Repository.Models;
 
@@ -25,7 +26,17 @@ public partial class NoteDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(local);Uid=sa;Pwd=1;Database=NoteDB;TrustServerCertificate=true");
+        => optionsBuilder.UseSqlServer(GetConnectionString());
+
+    public string GetConnectionString() {
+        string connectionString;
+        IConfiguration config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true)
+            .Build();
+        connectionString = config["ConnectionStrings:NoteDB"];
+        return connectionString;
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
