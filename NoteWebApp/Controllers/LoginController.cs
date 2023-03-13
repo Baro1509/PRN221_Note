082922e26de8751640c0918fed439cfb2e148306
vylt6 <luantuongvy13@gmail.com>
2023-03-13 12:42:38 +0700

    Get userId after login

--- NoteWebApp/Controllers/LoginController.cs
+++ NoteWebApp/Controllers/LoginController.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Cors;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using NoteWebApp.Request;
@@ -10,35 +9,42 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
-namespace NoteWebApp.Controllers {
+namespace NoteWebApp.Controllers
+{
     [Route("api/login")]
     [EnableCors]
     [ApiController]
-    public class LoginController : ControllerBase {
+    public class LoginController : ControllerBase
+    {
         private readonly UserRepository _userRepository;
         private readonly IConfiguration _config;
         private readonly IMapper _mapper;
-        
-        public LoginController(UserRepository userRepository, IConfiguration config, IMapper mapper) {
+
+        public LoginController(UserRepository userRepository, IConfiguration config, IMapper mapper)
+        {
             _userRepository = userRepository;
             _config = config;
             _mapper = mapper;
         }
 
         [HttpPost]
-        public IActionResult Login([FromBody]LoginRequest loginRequest) {
-            if (loginRequest == null || loginRequest.Email.IsNullOrEmpty() || loginRequest.Password.IsNullOrEmpty()) {
+        public IActionResult Login([FromBody] LoginRequest loginRequest)
+        {
+            if (loginRequest == null || loginRequest.Email.IsNullOrEmpty() || loginRequest.Password.IsNullOrEmpty())
+            {
                 return BadRequest();
             }
             var user = _userRepository.GetAll().Where(p => p.Email == loginRequest.Email).FirstOrDefault();
-            if (user == null || !BCrypt.Net.BCrypt.Verify(loginRequest.Password, user.Password)) {
+            if (user == null || !BCrypt.Net.BCrypt.Verify(loginRequest.Password, user.Password))
+            {
                 return Unauthorized();
             }
             var tokenString = GenerateJSONWebToken(user);
-            return Ok(new {token =  tokenString});
+            return Ok(new { token = tokenString, userId = user.Id });
         }
 
-        private string GenerateJSONWebToken(User user) {
+        private string GenerateJSONWebToken(User user)
+        {
             var claims = new[] {
                 new Claim(JwtRegisteredClaimNames.Sub, _config["Jwt:Subject"]),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
@@ -60,7 +66,8 @@ namespace NoteWebApp.Controllers {
 
         [HttpPost]
         [Route("/api/users/create")]
-        public IActionResult Create([FromBody] UserRequest request) {
+        public IActionResult Create([FromBody] UserRequest request)
+        {
             request.Password = BCrypt.Net.BCrypt.HashPassword(request.Password);
             _userRepository.Create(_mapper.Map<User>(request));
             var user = _userRepository.GetAll().Where(p => p.FirstName == request.FirstName).FirstOrDefault();
